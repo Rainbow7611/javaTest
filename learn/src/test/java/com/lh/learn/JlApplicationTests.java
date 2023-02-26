601b78d8bb9f9af9b8eebe7f349c009038d76548
Rainbow7611 <lihong52136@gmail.com>
2023-02-26 19:59:54 +0900

    v0.1
    v0.1

---
+++ learn/src/test/java/com/lh/learn/JlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lh.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
